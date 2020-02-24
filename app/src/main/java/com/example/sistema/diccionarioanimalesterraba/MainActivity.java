7a50330a79b7f81bd20f5c3838436ebe4b2e181d
esteban201483 <Esteban201483@gmail.com>
2020-02-24 14:43:46 -0600

    Funcional

---
+++ app/src/main/java/com/example/sistema/diccionarioanimalesterraba/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.sistema.diccionarioanimalesterraba;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    int index = 0;
+    float start = 0;
+    int upperBound = 40;
+    int lowerBound = 1;
+
+    String nombre = "animales";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        index = 1;
+
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e)
+        {
+
+        }
+
+        //Asigna la detección deslizamiento izquierdo y derecho
+        ImageView img = (ImageView) findViewById(R.id.imageView);
+
+        img.setOnTouchListener( new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+
+                if(event.getAction() == MotionEvent.ACTION_DOWN) //Cuando empieza el toque
+                {
+                    start = event.getX();
+                }
+                else
+                    if(event.getAction() == MotionEvent.ACTION_UP) //Cuando termina el toque
+                    {
+                        float end = event.getX();
+
+                        if(end - start < 0)
+                        {
+                            rightSlide(v);
+                        }
+                        else
+                        {
+                            leftSlide(v);
+                        }
+                    }
+
+                return true;
+            }
+        }
+
+        );
+
+
+
+    }
+
+    public void actualizarImagen(ImageView imageView)
+    {
+        String nombre_imagen = nombre + index;
+        int id_imagen = getResources().getIdentifier(nombre_imagen,"drawable",getPackageName());
+
+        imageView.setImageResource(id_imagen);
+    }
+
+    public void rightSlide(View img)
+    {
+        ImageView imageView = (ImageView) img;
+
+        if (index < upperBound) {
+            index++;
+            actualizarImagen(imageView);
+
+        }
+    }
+
+    public void leftSlide(View img)
+    {
+        ImageView imageView = (ImageView) img;
+
+        if (index > lowerBound) {
+            index--;
+            actualizarImagen(imageView);
+
+        }
+    }
+
+    public void irOpcion(int newIndex)
+    {
+        ImageView img = (ImageView) findViewById(R.id.imageView);
+        index = newIndex;
+        actualizarImagen(img);
+    }
+
+    public void displayMenuList(View v)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Indice");
+
+
+        String[] categorias = {"Portada", "Marinos", "Reptiles", "Voladores",
+        "¿?"};
+        builder.setItems(categorias, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:  irOpcion(1);    break;//Portada
+                    case 1:  irOpcion(5);    break;//Marinos
+                    case 2:  irOpcion(11);    break;//Reptiles
+                    case 3:  irOpcion(13);    break;//Voladores
+                    case 4:  irOpcion(23);    break;//¿?
+
+                }
+            }
+        });
+
+// create and show the alert dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+}
